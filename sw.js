ca87a84d5a0fdc014f01acfb51a7bf131164681c
trade2spend <trade2spend@gmail.com>
2026-05-29 07:06:46 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE = 't2s-v1';
+const SHELL = ['./index.html','./login.html','./welcome.html','./logo.png','./manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).catch(()=>{}));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if(e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
